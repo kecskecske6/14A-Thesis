c2b7d3ca4cfbde94880c79768dd645c1f2db7663
Levente011 <szebik.levente@gmail.com>
2021-11-15 20:19:56 +0100

    Login Page basic GUI and Google reCAPTCHA installiation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaV3Module  } from "ng-recaptcha";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +15,10 @@ import { LoginPageComponent } from './components/login-page/login-page.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    RecaptchaV3Module
   ],
-  providers: [],
+  providers: [{provide: RECAPTCHA_V3_SITE_KEY, useValue:"6Lc4wjcdAAAAAF-Yo1C2HrSThKcsrM8vFppMSOeT"}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
