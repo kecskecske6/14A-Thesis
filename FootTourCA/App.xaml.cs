35134d98c3305785f926d6cd638708a3e1ee14f2
kecskecske6 <kecskecske6@gmail.com>
2021-11-24 12:45:20 +0100

    .gitignore Added .gitignore, rename companion app

--- FootTourCA/App.xaml.cs
+++ FootTourCA/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace FootTour_Companion_App
+namespace FootTourCA
 {
     /// <summary>
     /// Interaction logic for App.xaml
