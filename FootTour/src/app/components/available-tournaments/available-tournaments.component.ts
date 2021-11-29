4777bd5f8958236700c95ebfb193eb06f0a4b7c6
Levente011 <szebik.levente@gmail.com>
2021-11-29 20:35:45 +0100

    Initialization

---
+++ FootTour/src/app/components/available-tournaments/available-tournaments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-available-tournaments',
+  templateUrl: './available-tournaments.component.html',
+  styleUrls: ['./available-tournaments.component.sass']
+})
+export class AvailableTournamentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
