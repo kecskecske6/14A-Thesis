7407fd9cdeb26135656314954935d10354dd7b42
Levente011 <szebik.levente@gmail.com>
2021-12-02 18:35:54 +0100

    Available tournaments html and basic style changes

--- FootTour/src/app/components/available-tournaments/available-tournaments.component.ts
+++ FootTour/src/app/components/available-tournaments/available-tournaments.component.ts
@@ -7,6 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvailableTournamentsComponent implements OnInit {
 
+  tournaments = [
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Győr',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    },
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Budapest',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    },
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Szeged',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    },
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Győr',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    },
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Győr',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    },
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Győr',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    },
+    {
+      name: 'Mikulás kupa',
+      organizer: 'Teszt Elek',
+      location: 'Győr',
+      start_date: '2021.12.24',
+      entry_fee: "10.000"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
