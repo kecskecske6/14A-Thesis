90e8a2bc7787d748dfe41771f1a6fbf3e8de4adf
kecskecske6 <kecskecske6@gmail.com>
2021-12-03 10:18:42 +0100

    Fixes New component for main page, some fixes

--- FootTour/src/app/app-routing.module.ts
+++ FootTour/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
 import { AvailableTournamentsComponent } from './components/available-tournaments/available-tournaments.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 const routes: Routes = [
+  { path: '', component: MainPageComponent },
   { path: 'login', component: LoginPageComponent },
   { path: 'register', component: RegistrationPageComponent},
-  { path: 'Availabletournaments', component: AvailableTournamentsComponent}
+  { path: 'database/tournaments', component: AvailableTournamentsComponent}
 ];
 
 @NgModule({
