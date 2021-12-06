64f49693543a98134fa6d124df828de18d5e4b3e
Levente011 <szebik.levente@gmail.com>
2021-12-06 17:39:52 +0100

    Tournament schedule component basic GUI

---
+++ FootTour/src/app/components/tournament-schedule/tournament-schedule.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tournament-schedule',
+  templateUrl: './tournament-schedule.component.html',
+  styleUrls: ['./tournament-schedule.component.sass']
+})
+export class TournamentScheduleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
