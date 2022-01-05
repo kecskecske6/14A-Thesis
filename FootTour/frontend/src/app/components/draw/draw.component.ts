ce1f5199991c6af61b0d242bc75e2002dab82f9b
kecskecske6 <kecskecske6@gmail.com>
2022-01-05 13:18:53 +0100

    Registration fixes and draw Database additions and draw enhancements

--- FootTour/frontend/src/app/components/draw/draw.component.ts
+++ FootTour/frontend/src/app/components/draw/draw.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-draw',
@@ -8,8 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class DrawComponent implements OnInit {
 
   tournament = {
-    type: ''
+    type: 'Egyenes kieséses',
+    teamsCount: 8,
+    groupsAmount: 4
   }
+
+  teams = [
+    'ittASöröm',
+    'ittASöröm2',
+    'ittASöröm3',
+    'ittASöröm4',
+    'ittASöröm5',
+    'ittASöröm6',
+    'ittASöröm7',
+    'ittASöröm8'
+  ]
 
   types = [
     {
@@ -17,19 +31,35 @@ export class DrawComponent implements OnInit {
     },
     {
       name: 'Csoportkör és kieséses'
-    },
-    {
-      name: 'Liga'
     }
   ]
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  logging() {
-    console.log(this.tournament);
+  set(value: string) {
+    this.tournament.type = value;
+  }
+
+  draw() {
+    if (this.tournament.type == '') return alert('Kérem, válasszon típust!');
+    const groups: string[][] = [];
+    for (let i = 0; i < this.tournament.teamsCount / this.tournament.groupsAmount; i++) groups.push([]);
+    for (let i = 0; i < this.teams.length; i++) {
+      let team; 
+      let doAgain;
+      do {
+        team = this.teams[Math.random() * this.teams.length];
+        doAgain = false;
+        for (let j = 0; j < groups.length; j++) {
+          if (groups[j].includes(team)) doAgain = true;
+        }
+      } while (doAgain);
+      groups[Math.floor(i / this.tournament.groupsAmount)].push(team);
+    }
+    this.router.navigate(['/schedule']);
   }
 
 }
