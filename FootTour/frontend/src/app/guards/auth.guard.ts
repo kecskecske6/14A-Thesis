3e67d68b54dca748982c94a7ae6791290a792f1f
Levente011 <szebik.levente@gmail.com>
2022-01-16 21:01:01 +0100

    Stay logged in Fixes #71

--- FootTour/frontend/src/app/guards/auth.guard.ts
+++ FootTour/frontend/src/app/guards/auth.guard.ts
@@ -10,7 +10,7 @@ export class AuthGuard implements CanActivate {
   
   canActivate(){
       if(!this.authService.isLoggedIn()){
-        this.router.navigate(['/register']);
+        this.router.navigate(['/login']);
         return false;
       }else{
         return true;
