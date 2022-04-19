58721b4acd6afe142f0cd99aaf7aee99a8b98f17
kecskecske6 <kecskecske6@gmail.com>
2022-04-19 14:11:05 +0200

    Draw final version

--- frontend/src/app/components/referee-match-report/referee-match-report.component.ts
+++ frontend/src/app/components/referee-match-report/referee-match-report.component.ts
@@ -14,6 +14,7 @@ import { TeamstoGroupsModel } from 'src/app/models/TeamstoGroups';
 import { TournamentService } from 'src/app/services/tournament.service';
 import { TournamentModel } from 'src/app/models/Tournament';
 import { TeamModel } from 'src/app/models/Team';
+import { TeamService } from 'src/app/services/team.service';
 
 @Component({
   selector: 'app-referee-match-report',
@@ -48,7 +49,7 @@ export class RefereeMatchReportComponent implements OnInit {
 
   constructor(private matchService: MatchService,
     private router: Router,
-    private authService: AuthService, private groupService: GroupService, private userService: UserService, private teamstoGroupsService: TeamstoGroupsService, private tournamentService: TournamentService) { }
+    private authService: AuthService, private groupService: GroupService, private userService: UserService, private teamstoGroupsService: TeamstoGroupsService, private tournamentService: TournamentService, private teamService: TeamService) { }
 
   ngOnInit(): void {
     this.getMatchById();
@@ -155,23 +156,117 @@ export class RefereeMatchReportComponent implements OnInit {
                 }
               }
             }
-            else {
+            else if (result[0].code.startsWith('G')) {
               if (result.every(m => m.team1Goals != null)) {
                 this.groupService.getByType(this.tournamentId, 'GS%').subscribe(
                   resultGroups => {
                     this.teamstoGroupsService.getByTournamentId(this.tournamentId).subscribe(
                       resultTTG => {
-                        const ttg: MatchModel[][] = [];
-                        const teamIds = [];
+                        const teamIds: number[] = [];
+                        const teams: (TeamModel | undefined)[][] = [];
                         resultGroups.forEach(g => {
                           resultTTG.filter(t2g => t2g.groupId == g.id).forEach(t2g => teamIds.push(t2g.teamId));
                         });
-                        const groups: TeamModel[][] = [];
-                        ttg.forEach(mg => {
-                          mg.forEach(m => {
-
-                          });
-                        });
+                        this.teamService.getAllByTournamentId(this.tournamentId).subscribe(
+                          resultTeams => {
+                            const names = ['A', 'B', 'C', 'D'];
+                            if (this.tournament.teamsCount == 32) names.push(...['E', 'F', 'G', 'H']);
+                            names.forEach(n => {
+                              const teamsCurrent: (TeamModel | undefined)[] = [];
+                              resultTTG.filter(t2g => t2g.groupId == resultGroups.find(g => g.name == `GS${n}`)?.id).forEach(t2g => teamsCurrent.push(resultTeams.find(t => t.id == t2g.teamId)));
+                              teams.push(teamsCurrent);
+                            });
+                            const orderedTeams: any[][] = [];
+                            teams.forEach(g => {
+                              let teamsCurrent: any[] = [];
+                              g.forEach(t => {
+                                const numberOfWins = result.filter(m => m.team1Goals != null && m.team2Goals != null && (m.team1Id == t?.id && m.team1Goals > m.team2Goals || m.team2Id == t?.id && m.team2Goals > m.team1Goals)).length;
+                                const numberOfDraws = result.filter(m => m.team1Goals != null && m.team2Goals != null && ((m.team1Id == t?.id || m.team2Id == t?.id) && m.team2Goals == m.team1Goals)).length;
+                                const matches = result.filter(m => (m.team1Id == t?.id || m.team2Id == t?.id));
+                                let numberOfGoals = 0;
+                                let numberOfReceivedGoals = 0;
+                                matches.forEach(m => {
+                                  if (m.team1Id == t?.id) {
+                                    numberOfGoals += m.team1Goals ?? 0;
+                                    numberOfReceivedGoals += m.team2Goals ?? 0;
+                                  }
+                                  else {
+                                    numberOfGoals += m.team2Goals ?? 0;
+                                    numberOfReceivedGoals += m.team1Goals ?? 0;
+                                  }
+                                });
+                                teamsCurrent.push({
+                                  id: t?.id,
+                                  wins: numberOfWins,
+                                  draws: numberOfDraws,
+                                  goalsFor: numberOfGoals,
+                                  goalsAgainst: numberOfReceivedGoals,
+                                  points: numberOfWins * 3 + numberOfDraws
+                                });
+                              });
+                              teamsCurrent = teamsCurrent.sort((a, b) => {
+                                if (b.points - a.points != 0) return b.points - a.points > 0 ? 1 : -1;
+                                if ((b.goalsFor - b.goalsAgainst) - (a.goalsFor - a.goalsAgainst) != 0) return (b.goalsFor - b.goalsAgainst) - (a.goalsFor - a.goalsAgainst) > 0 ? 1 : -1;
+                                if (b.goalsFor - a.goalsFor != 0) return b.goalsFor - a.goalsFor > 0 ? 1 : -1;
+                                if (b.wins - a.wins != 0) return b.wins - a.wins > 0 ? 1 : -1;
+                                return 0;
+                              });
+                              orderedTeams.push(teamsCurrent.slice(0, 2));
+                            });
+                            for (let i = 0; i < orderedTeams.length; i++) {
+                              let groupModel = new GroupModel();
+                              groupModel.name = this.tournament.teamsCount == 32 ? `R16${i}` : `QF${i}`;
+                              groupModel.tournamentId = this.tournamentId;
+                              this.groupService.create(groupModel).subscribe(
+                                createdGroups => {
+                                  let matchModel = new MatchModel();
+                                  matchModel.code = this.tournament.teamsCount == 32 ? `R16${i}-1` : `QF${i}-1`;
+                                  matchModel.groupId = createdGroups.id;
+                                  matchModel.refereeId = this.referees[Math.floor(Math.random() * this.referees.length)].id;
+                                  matchModel.team1Goals = null;
+                                  matchModel.team1Id = orderedTeams[i][0].id;
+                                  matchModel.team2Goals = null;
+                                  matchModel.team2Id = i % 2 == 0 ? orderedTeams[i + 1][1].id : orderedTeams[i - 1][1].id;
+                                  this.matchService.create(matchModel).subscribe(
+                                    result3 => {
+                                      if (this.tournament.knockoutMatches == 2) {
+                                        matchModel = new MatchModel();
+                                        matchModel.code = this.tournament.teamsCount == 32 ? `R16${i}-2` : `QF${i}-2`;
+                                        matchModel.groupId = createdGroups.id;
+                                        matchModel.refereeId = this.referees[Math.floor(Math.random() * this.referees.length)].id;
+                                        matchModel.team1Goals = null;
+                                        matchModel.team1Id = result3.team2Id;
+                                        matchModel.team2Goals = null;
+                                        matchModel.team2Id = result3.team1Id;
+                                        this.matchService.create(matchModel).subscribe(
+                                          result4 => { },
+                                          error => console.log(error)
+                                        );
+                                      }
+                                      let teamstoGroupsModel = new TeamstoGroupsModel();
+                                      teamstoGroupsModel.groupId = createdGroups.id;
+                                      teamstoGroupsModel.teamId = result3.team1Id;
+                                      this.teamstoGroupsService.create(teamstoGroupsModel).subscribe(
+                                        result4 => { },
+                                        error => console.log(error)
+                                      );
+                                      teamstoGroupsModel = new TeamstoGroupsModel();
+                                      teamstoGroupsModel.groupId = createdGroups.id;
+                                      teamstoGroupsModel.teamId = result3.team2Id;
+                                      this.teamstoGroupsService.create(teamstoGroupsModel).subscribe(
+                                        result4 => { },
+                                        error => console.log(error)
+                                      );
+                                    },
+                                    error => console.log(error)
+                                  );
+                                },
+                                error => console.log(error)
+                              );
+                            }
+                          },
+                          error => console.log(error)
+                        );
                       },
                       error => console.log(error)
                     );
